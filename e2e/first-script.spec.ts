3b825be8feafa46ff8933099c13e1cb751acec89
unknown <boda.alexandruflorin@yahoo.com>
2025-08-15 17:47:33 +0300

    create script and store credentials in .env

---
+++ e2e/first-script.spec.ts
@@ -0,0 +1,57 @@
+import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
+dotenv.config();
+const username = process.env.MY_USERNAME;
+const password = process.env.MY_PASSWORD;
+
+console.log(username);
+console.log(password);
+
+test('Page has expected title', async ({ page }) => {
+  await page.goto('https://www.collaboard.app/');
+
+  await expect(page).toHaveTitle(/Online Whiteboard/);
+});
+
+test('Click the login button', async ({ page }) => {
+  await page.goto('https://www.collaboard.app/');
+
+  await page.getByRole('link', { name: 'Login' }).click();
+  await expect(page.getByRole('heading', { name: 'Welcome' })).toBeVisible();
+});
+
+test('Click Sign in', async ({ page }) => {
+  await page.goto('https://www.collaboard.app/');
+
+  await page.getByRole('link', { name: 'Login' }).click();
+  await expect(page.getByRole('heading', { name: 'Welcome' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await expect(page.getByRole('heading', { name: 'Sign in' })).toBeVisible();
+});
+
+test('Input email and password and login', async ({ page }) => {
+  console.log(username);
+  console.log(password);
+
+  await page.goto('https://www.collaboard.app/');
+  
+  await page.getByRole('link', { name: 'Login' }).click();
+  await expect(page.getByRole('heading', { name: 'Welcome' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await expect(page.getByRole('heading', { name: 'Sign in' })).toBeVisible();
+
+  await expect(page.getByRole('heading', { name: 'Sign in' })).toBeVisible();
+  await page.getByRole('textbox', { name: 'Username' }).fill(username!);
+
+  await page.getByRole('textbox', { name: 'Username' }).blur();
+  await page.waitForTimeout(200);
+
+  await page.getByRole('button', { name: 'Continue with email' }).click();
+
+  await page.getByRole('textbox', { name: 'Password' }).fill(password!);
+  await page.getByRole('button', { name: 'Continue' }).click();
+
+  await expect(page.getByRole('heading', { name: 'Projects' })).toBeVisible();
+});
